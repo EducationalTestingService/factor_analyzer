7252a03172019b9fff3e54e8f671ef1f01f00258
Jeremy Biggs <jbiggs@ets.org>
2017-12-06 15:30:38 -0500

    adding __init__ back

---
+++ factor_analyzer/__init__.py
@@ -0,0 +1,8 @@
+# License: GLP2
+"""
+:author: Jeremy Biggs (jbiggs@ets.org)
+:organization: ETS
+"""
+
+from .factor_analyzer import (FactorAnalyzer,
+                              calculate_bartlett_sphericity)
