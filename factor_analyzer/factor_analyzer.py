Add phi and structure matrices for oblique rotations - Add phi and structure matrices for oblique rotations - Fix minor bug in rotation matrices - Minor update to tests

factor_analyzer/factor_analyzer.py
@@ -19,7 +19,7 @@ from scipy.stats import chi2
 from scipy.optimize import minimize
 
 from factor_analyzer.rotator import Rotator
-from factor_analyzer.rotator import POSSIBLE_ROTATIONS
+from factor_analyzer.rotator import POSSIBLE_ROTATIONS, OBLIQUE_ROTATIONS
 
 
 def covariance_to_correlation(m):
@@ -263,6 +263,9 @@ class FactorAnalyzer:
         self.log_warnings = log_warnings
 
         # default matrices to None
+        self.phi = None
+        self.structure = None
+
         self.corr = None
         self.loadings = None
         self.rotation_matrix = None
@@ -741,32 +744,53 @@ class FactorAnalyzer:
                                             bounds,
                                             method)
 
+        # only used if we do an oblique rotations
+        phi = None
+        structure = None
+
         # default rotation matrix to None
         rotation_mtx = None
 
         # whether to rotate the loadings matrix
         if rotation is not None:
 
-            if loadings.shape[1] == 1:
+            if loadings.shape[1] > 1:
+                rotator = Rotator()
+                loadings, rotation_mtx, phi = rotator.rotate(loadings,
+                                                             rotation,
+                                                             normalize=normalize,
+                                                             **kwargs)
+
+                if rotation != 'promax':
+
+                    # update the rotation matrix for everything except promax
+                    rotation_mtx = np.linalg.inv(rotation_mtx).T
+
+            else:
                 warnings.warn('No rotation will be performed when '
                               'the number of factors equals 1.')
-            else:
-                rotator = Rotator()
-                loadings, rotation_mtx = rotator.rotate(loadings,
-                                                        rotation,
-                                                        normalize=normalize,
-                                                        **kwargs)
 
         if n_factors > 1:
 
             # update loading signs to match column sums
             # this is to ensure that signs align with R
-            # results
             signs = np.sign(loadings.sum(0))
             signs[(signs == 0)] = 1
             loadings = pd.DataFrame(np.dot(loadings, np.diag(signs)),
                                     index=loadings.index,
                                     columns=loadings.columns)
+
+            if phi is not None:
+
+                # update phi, if it exists -- that is, if the rotation is oblique
+                phi = np.dot(np.dot(np.diag(signs), phi), np.diag(signs))
+
+                # create the structure matrix for any oblique rotation
+                structure = np.dot(loadings, phi) if rotation in OBLIQUE_ROTATIONS else None
+                structure = pd.DataFrame(structure, columns=loadings.columns, index=loadings.index)
+
+        self.phi = phi
+        self.structure = structure
 
         self.corr = df.corr()
         self.loadings = loadings

factor_analyzer/rotator.py
@@ -13,11 +13,11 @@ import numpy as np
 import scipy as sp
 import pandas as pd
 
+ORTHO_ROTATIONS = ['varimax', 'oblimax', 'quartimax', 'equamax']
 
-POSSIBLE_ROTATIONS = ['varimax', 'promax',
-                      'oblimax', 'quartimax',
-                      'oblimin', 'quartimin',
-                      'equamax']
+OBLIQUE_ROTATIONS = ['promax', 'oblimin', 'quartimin']
+
+POSSIBLE_ROTATIONS = ORTHO_ROTATIONS + OBLIQUE_ROTATIONS
 
 
 class Rotator:
@@ -291,12 +291,15 @@ class Rotator:
             gradient = -np.dot(np.dot(new_loadings.T, obj_t['grad']),
                                np.linalg.inv(new_rotation_matrix)).T
 
+        # calculate phi
+        phi = np.dot(rotation_matrix.T, rotation_matrix)
+
         # convert loadings matrix to data frame
         loadings = pd.DataFrame(new_loadings,
                                 columns=column_names,
                                 index=index_names)
 
-        return loadings, rotation_matrix
+        return loadings, rotation_matrix, phi
 
     def orthogonal(self,
                    loadings,
@@ -576,12 +579,15 @@ class Rotator:
 
         rotation_mtx = sp.dot(rotation_mtx, coef)
 
+        coef_inv = np.linalg.inv(coef)
+        phi = np.dot(coef_inv, coef_inv.T)
+
         # convert loadings matrix to data frame
         loadings = pd.DataFrame(z,
                                 columns=column_names,
                                 index=index_names)
 
-        return loadings, rotation_mtx
+        return loadings, rotation_mtx, phi
 
     def rotate(self, loadings, method='varimax', **kwargs):
         """
@@ -622,6 +628,12 @@ class Rotator:
             If the `method` is not in the list of
             acceptable methods.
         """
+
+        # default phi to None
+        # it will only be calculated
+        # for oblique rotations
+        phi = None
+
         method = method.lower()
         if method == 'varimax':
             (new_loadings,
@@ -629,7 +641,7 @@ class Rotator:
 
         elif method == 'promax':
             (new_loadings,
-             new_rotation_mtx) = self.promax(loadings, **kwargs)
+             new_rotation_mtx, phi) = self.promax(loadings, **kwargs)
 
         elif method == 'oblimax':
             (new_loadings,
@@ -641,11 +653,11 @@ class Rotator:
 
         elif method == 'oblimin':
             (new_loadings,
-             new_rotation_mtx) = self.oblique(loadings, self._oblimin_obj, **kwargs)
+             new_rotation_mtx, phi) = self.oblique(loadings, self._oblimin_obj, **kwargs)
 
         elif method == 'quartimin':
             (new_loadings,
-             new_rotation_mtx) = self.oblique(loadings, self._quartimin_obj, **kwargs)
+             new_rotation_mtx, phi) = self.oblique(loadings, self._quartimin_obj, **kwargs)
 
         elif method == 'equamax':
             (new_loadings,
@@ -655,4 +667,4 @@ class Rotator:
             raise ValueError("The value for `method` must be one of the "
                              "following: {}.".format(', '.join(POSSIBLE_ROTATIONS)))
 
-        return new_loadings, new_rotation_mtx
+        return new_loadings, new_rotation_mtx, phi