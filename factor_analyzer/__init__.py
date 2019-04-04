08ac9b4654f62695fa6ac0f3622193f9308d9ec9
jbiggsets <jbiggs@ets.org>
2019-04-04 09:40:34 -0400

    updating init and meta.yaml and removing analyze

--- factor_analyzer/__init__.py
+++ factor_analyzer/__init__.py
@@ -14,10 +14,16 @@ from .confirmatory_factor_analyzer import (ConfirmatoryFactorAnalyzer,
                                            ModelSpecificationParser,
                                            ModelSpecification)
 
-from .utils import (fill_lower_diag,
+from .utils import (cov,
+                    corr,
+                    fill_lower_diag,
+                    impute_values,
+                    smc,
+                    partial_correlations,
                     merge_variance_covariance,
                     duplication_matrix,
                     duplication_matrix_pre_post,
+                    covariance_to_correlation,
                     commutation_matrix,
                     get_symmetric_lower_idxs,
                     get_symmetric_upper_idxs)
