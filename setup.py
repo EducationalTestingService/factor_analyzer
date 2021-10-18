10f4e563fd67831d6cb5eddb48e99ca17560aaca
Nitin Madnani <nmadnani@ets.org>
2021-10-18 11:02:51 -0400

    Update version numbers

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def requirements():
 
 
 setup(name='factor_analyzer',
-      version='0.3.2',
+      version='0.4.0',
       description='A Factor Analysis class',
       long_description=readme(),
       keywords='factor analysis',
